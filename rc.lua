1057df14df12c09c3f97fa0fb124ed0e71c6ae3f
Gorka Elexgaray <gelexgaray@yahoo.es>
2023-11-16 14:14:55 +0100

    change vertical and horizontal split signs

--- rc.lua
+++ rc.lua
@@ -111,15 +111,15 @@ local vscode       = string.format('code --no-sandbox --user-data-dir="%s/.code"
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
-    --awful.layout.suit.max.fullscreen,
+    awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
+    --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.floating,
     --awful.layout.suit.tile,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.max,
     --awful.layout.suit.magnifier,
